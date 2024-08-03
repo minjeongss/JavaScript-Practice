fdadfde2ec81253fb028597f3ef6643048ce7ec9
minjeongss <dotoriido@gmail.com>
2024-08-03 20:14:26 +0900

    feat: pagination 기능 구현 👊

--- Slide/slide.js
+++ Slide/slide.js
@@ -19,6 +19,20 @@ $btns.forEach((item) => {
     }, 0);
   });
 });
+$dots.forEach((item) => {
+  item.addEventListener("click", () => {
+    const $sliderUl = document.querySelector(".slider-ul");
+
+    // $sliderUl.classList.add("move");
+    $sliderUl.classList.remove("move");
+
+    //TODO 비동기 처리 없이 애니메이션 효과가 나타나도록 변경
+    //TODO 이전 슬라이드 부분이 이동할 때 반짝이는 문제 수정 필요(애니메이션 요소 충돌이 원인)
+    setTimeout(() => {
+      $sliderUl.classList.add("move");
+    }, 0);
+  });
+});
 //무한 슬라이드: 이전 버튼
 $prevBtn.addEventListener("click", () => {
   //변화된 형태 확인을 위해 전역이 아닌 지역으로 선언
@@ -53,7 +67,43 @@ $nextBtn.addEventListener("click", () => {
 
 //페이지네이션
 const handleList = (item) => {
-  //item.innerText
+  //sliderIndex, paginationIndex 차이
+  //+: 2/1 prev처럼
+  //-: 2/4 next처럼
+  const $sliderIndex = Number(
+    document.querySelectorAll(".slider")[0].innerText
+  );
+  const $paginationIndex = Number(item.innerText);
+  const diffIndex = $sliderIndex - $paginationIndex;
+  //! 마이너스 될 수 있음
+
+  switch ($paginationIndex) {
+    case 1:
+      console.log("diff", diffIndex);
+      //2라면 2341 > 1234
+      //3이라면 3412 > 2341 > 1234
+      for (let i = 0; i < diffIndex; i++) {
+        const $slider = document.querySelectorAll(".slider");
+        const $sliderUl = document.querySelector(".slider-ul");
+        //삭제
+        $sliderUl.removeChild($sliderUl.lastElementChild);
+
+        //할당
+        let currSlider = document.createElement("li");
+        currSlider.innerHTML = $slider[$slider.length - 1].innerHTML;
+        currSlider.classList.add("slider");
+        $sliderUl.prepend(currSlider);
+
+        console.log(currSlider);
+      }
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+  }
 };
 $dots.forEach((item) => {
   item.addEventListener("click", () => {
